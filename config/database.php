6e584ef8bba4a1b3e02f7137207abae2006fdf9e
esentino <esntino@gmail.com>
2015-12-28 22:52:54 +0100

    Kolejny rozdzial tutoriala

--- config/database.php
+++ config/database.php
@@ -55,8 +55,8 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'ubuntu'),
-            'username'  => env('DB_USERNAME', ''),
+            'database'  => env('DB_DATABASE', 'circle_test'),
+            'username'  => env('DB_USERNAME', 'ubuntu'),
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
